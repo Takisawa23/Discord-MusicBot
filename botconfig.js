9f08dd6032146492aab1681c7fb78bddf648fe78
Takisawa23 <109555729+Takisawa23@users.noreply.github.com>
2022-07-18 16:25:36 -0500

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   Admins: ["UserID", "UserID"], // Admins of the bot, I don't know what this do. -Darren.
   ExpressServer: true, // If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || ">", // Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "T", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
+  Token: process.env.Token || "ODg3NDg0NjEyODE1NzQ5MjIx.Gom7LY.jT7SNk6Rb0BzUXzEdzcv8e0FmL8uRwtfVNOxtg", // Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", // Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
